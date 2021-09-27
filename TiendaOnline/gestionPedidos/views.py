b0f840845fdd3666d8a2e46a52d6604ff7f5ebc7
Ileana Chamorro <ic.chamorro07@gmail.com>
2021-09-27 15:40:10 -0300

    Formularios III

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -9,12 +9,25 @@ def busqueda_productos(request):
 def buscar(request):
     #Si el usuario manda el formulario vacio
     if request.GET["prd"]:
-       producto=request.GET['prd']
+        producto=request.GET["prd"]
 
-       articulos=Articulos.objects.filter(nombre__icontains=producto)
+        #Limitar caracteres de busqueda
+        if len(producto)>20:
+            mensaje="Texto demasiado largo"
+        else:
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
 
-       return render(request, "resultado_busqueda.html", {'articulos':articulos, 'query':producto})
+            return render(request, "resultados_busqueda.html", {'articulos':articulos, 'query':producto})
     else:
         mensaje="Para obtener un resultado debes introducir el nombre del producto"
 
     return HttpResponse(mensaje)
+
+
+#Formulario de Contacto
+def contacto(request):  
+    #En caso que detecte el post
+    if request.method=="POST":
+        return render(request, "saludos.html")
+        
+    return render(request, "formcontacto.html")
